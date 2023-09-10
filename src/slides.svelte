4e8a2654f22e58983983b8b4ce653a5fe17deac4
* <creastatic@gmail.com>
2023-09-10 13:31:29 -0300

    All done, I'll touch if there are some issues

--- src/slides.svelte
+++ src/slides.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { Presentation, Slide, Stack, Step } from '@components'
-  import {InfoIcon, LayersIcon, ThumbsUpIcon, ToolIcon, UsersIcon, AwardIcon, EyeIcon, DollarSignIcon} from 'svelte-feather-icons'
+  import {InfoIcon, LayersIcon, ThumbsUpIcon, ToolIcon, UsersIcon, AwardIcon, EyeIcon, DollarSignIcon, FilterIcon, LogInIcon, LogOutIcon, HelpCircleIcon, Share2Icon} from 'svelte-feather-icons'
 </script>
 
 <Presentation>
@@ -12,8 +12,8 @@
           alt="" />
       </div>
       <div style="text-shadow: 0 10px 20px black;"
-        class="bg-[#758BFD] w-full flex items-center justify-center font-bold m-10 rounded-tr-full rounded-bl-full text-6xl">
-        CREASTATIC
+        class="bg-[#758BFD] w-full flex items-center justify-center font-bold m-10 rounded-tr-full rounded-bl-full">
+        <p class="text-6xl">CREASTATIC</p> <sup class="text-sm text-[#758BFD]">&copy</sup>
       </div>
     </div>
     <div class="p-10">
@@ -81,7 +81,7 @@
     <div class="">
       <div class="flex flex-col justify-center scroll-x-scroll">
         <h2 style="text-shadow: 0 10px 20px blue;"
-        class="font-bold m-10 flex justify-center items-center text-blue-600 gap-5 text-5xl">
+          class="font-bold m-10 flex justify-center items-center text-blue-600 gap-5 text-5xl">
           <AwardIcon size="40" />MISSÃO
         </h2>
         <div class="p-5 m-10 rounded-tr-full rounded-bl-full bg-[#758BFD]">
@@ -97,12 +97,12 @@
     <div class="">
       <div class="flex flex-col justify-center scroll-x-scroll">
         <h2 style="text-shadow: 0 10px 20px blue;"
-        class="font-bold m-10 flex justify-center items-center text-blue-600 gap-5 text-5xl" >
+          class="font-bold m-10 flex justify-center items-center text-blue-600 gap-5 text-5xl">
           <EyeIcon size="40" />VISÃO
         </h2>
         <div class="p-5 m-10 rounded-tr-full rounded-bl-full bg-[#758BFD]">
           <h3 style="text-shadow: 0 10px 20px black;" class="p-5 text-1xl">
-            Estar dentre as principais empresas de desenvolvimento web, assim trazendo relevância a marca
+            ESTAR DENTRE AS PRINCIPAIS EMPRESAS de desenvolvimento web, assim trazendo relevância a marca
           </h3>
         </div>
       </div>
@@ -112,7 +112,7 @@
     <div class="">
       <div class="flex flex-col justify-center scroll-x-scroll">
         <h2 style="text-shadow: 0 10px 20px blue;"
-        class="font-bold m-10 flex justify-center items-center text-blue-600 gap-5 text-5xl" >
+          class="font-bold m-10 flex justify-center items-center text-blue-600 gap-5 text-5xl">
           <DollarSignIcon size="40" />VALORES
         </h2>
         <div class="p-5 m-10 rounded-tr-full rounded-bl-full bg-[#758BFD]">
@@ -124,111 +124,166 @@
     </div>
   </Slide>
   
-    <Slide animate>
+  <Slide animate>
     <div class="">
       <div class="flex flex-col justify-center scroll-x-scroll">
         <h2 style="text-shadow: 0 10px 20px blue;"
-        class="font-bold m-10 flex justify-center items-center text-blue-600 gap-5 text-5xl" >
-          <LayersIcon size="40" />SWOT INTERNAL
+          class="font-bold m-10 flex justify-center items-center text-blue-600 gap-5 text-5xl">
+          <LogInIcon size="40" />SWOT INTERNAL
         </h2>
-              <div class="flex justify-center items-center">
-        <div
-          class="flex flex-col justify-center items-center flex rounded-tr-full rounded-bl-full bg-[#758BFD]">
-          <h1 class="p-5 text-5xl">STRENGTHS</h1>
-          <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-3xl flex flex-col gap-5">
-            <li>1. COMPROMETIMENTO</li>
-            <li>2. PROFISSIONALISMO</li>
-            <li>3. ADAPTAPTABILIDADE</li>
-            <li>4. MODERNIDADE</li>
-          </ol>
+        <div class="flex justify-center items-center">
+          <div class="flex flex-col justify-center items-center flex rounded-tr-full rounded-bl-full bg-[#758BFD]">
+            <h1 class="p-5 text-5xl">STRENGTHS</h1>
+            <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-3xl flex flex-col gap-5">
+              <li>1. COMPROMETIMENTO</li>
+              <li>2. PROFISSIONALISMO</li>
+              <li>3. ADAPTAPTABILIDADE</li>
+              <li>4. MODERNIDADE</li>
+            </ol>
+          </div>
+          
+          <div class="flex flex-col justify-center items-center flex rounded-tr-full rounded-bl-full bg-[#758BFD]">
+            <h1 class="p-5 text-5xl">WEAKNESS</h1>
+            <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-3xl flex flex-col gap-5">
+              <li>1. ACESSIBILIDADE</li>
+              <li>2. COMUNICAÇÃO</li>
+              <li>3. COMPATIBILIDADE</li>
+              <li>4. CONFIABILIDADE</li>
+            </ol>
+          </div>
+          
         </div>
-        
-        <div
-          class="flex flex-col justify-center items-center flex rounded-tr-full rounded-bl-full bg-[#758BFD]">
-          <h1 class="p-5 text-5xl">WEAKNESS</h1>
-          <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-3xl flex flex-col gap-5">
-            <li>1. ACESSIBILIDADE</li>
-            <li>2. COMUNICAÇÃO</li>
-            <li>3. COMPATIBILIDADE</li>
-            <li>4. CONFIABILIDADE</li>
-          </ol>
-        </div>
-        
-      </div>
       </div>
     </div>
   </Slide>
-      <Slide animate>
+  <Slide animate>
     <div class="">
       <div class="flex flex-col justify-center scroll-x-scroll">
         <h2 style="text-shadow: 0 10px 20px blue;"
-        class="font-bold m-10 flex justify-center items-center text-blue-600 gap-5 text-5xl" >
-          <LayersIcon size="40" />SWOT EXTERNAL
+          class="font-bold m-10 flex justify-center items-center text-blue-600 gap-5 text-5xl">
+          <LogOutIcon size="40" />SWOT EXTERNAL
         </h2>
-              <div class="flex justify-center items-center">
-        <div
-          class="flex flex-col justify-center items-center flex rounded-tr-full rounded-bl-full bg-[#758BFD]">
-          <h1 class="p-5 text-5xl">OPORTUNITIES</h1>
-          <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-3xl flex flex-col gap-5">
-            <li>1. LEADS</li>
-            <li>2. CONTRATAÇÕES</li>
-            <li>3. EVOLUÇÃO</li>
-            <li>4. INVESTIMENTO</li>
-          </ol>
+        <div class="flex justify-center items-center">
+          <div class="flex flex-col justify-center items-center flex rounded-tr-full rounded-bl-full bg-[#758BFD]">
+            <h1 class="p-5 text-5xl">OPORTUNITIES</h1>
+            <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-3xl flex flex-col gap-5">
+              <li>1. LEADS</li>
+              <li>2. CONTRATAÇÕES</li>
+              <li>3. EVOLUÇÃO</li>
+              <li>4. INVESTIMENTO</li>
+            </ol>
+          </div>
+          
+          <div class="flex flex-col justify-center items-center flex rounded-tr-full rounded-bl-full bg-[#758BFD]">
+            <h1 class="p-5 text-5xl">THREATS</h1>
+            <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-3xl flex flex-col gap-5">
+              <li>1. STARTUPS</li>
+              <li>2. CONCORRÊNCIA</li>
+              <li>3. MUDANÇAS TECNOLÓGICAS</li>
+              <li>4. DECADÊNCIA DIGITAL</li>
+            </ol>
+          </div>
+          
         </div>
-        
-        <div
-          class="flex flex-col justify-center items-center flex rounded-tr-full rounded-bl-full bg-[#758BFD]">
-          <h1 class="p-5 text-5xl">THREATS</h1>
-          <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-3xl flex flex-col gap-5">
-            <li>1. STARTUPS</li>
-            <li>2. CONCORRÊNCIA</li>
-            <li>3. MUDANÇAS TECNOLÓGICAS</li>
-            <li>4. DECADÊNCIA DIGITAL</li>
-          </ol>
-        </div>
-        
-      </div>
       </div>
     </div>
   </Slide>
   
   <Slide animate>
-    <h2 class="font-bold m-10 flex justify-center items-center text-[#08FFD0] gap-5 text-5xl">
+    <h2 style="text-shadow: 0 10px 20px green;"
+      class="font-bold m-10 flex justify-center items-center text-green-600 gap-5 text-5xl">
+      <FilterIcon size="40" />FUNIL DE VENDAS
+    </h2>
+    <div class="flex justify-center items-center relative">
+      <p class="absolute underline top-0 text-2xl left-0 p-5">topo do funil</p>
+      <p class="absolute bottom-0 underline text-2xl left-0 p-5">fundo do funil</p>
+      <div class="flex flex-col justify-around items-center p-5 flex rounded-br-full rounded-bl-full bg-[#758BFD]">
+        <h2 class="p-5 text-4xl">LEADS -> NECESSIDADE</h2>
+        <h2 class="p-5 text-4xl">
+          VISITANTES -> INTERESSE
+        </h2>
+        <h2 class="p-5 text-4xl">POSSÍVEIS COMPRADORES -> BENEFÍCIOS</h2>
+        <h2 class="p-5 text-4xl">COMPRADORES -> NEGOCIAÇÃO</h2>
+        <h2 class="p-5 text-4xl">CLIENTES -> LEALDADE</h2>
+      </div>
+    </div>
+  </Slide>
+  <Slide animate>
+    <h2 style="text-shadow: 0 10px 20px green;"
+      class="font-bold m-10 flex justify-center items-center text-green-600 gap-5 text-5xl">
       <ToolIcon size="40" />ESTRUTURA
     </h2>
-    <Stack class="flex flex-col items-center justify-center">
-      <div class="flex">
-        <div
-          class="flex flex-col justify-center items-center p-5 flex m-10 rounded-tr-full rounded-bl-full bg-[#758BFD]">
-          <h1 class="p-5 text-5xl">SETOR DE PRODUÇÃO</h1>
-          <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-1xl flex flex-col gap-5">
-            <li>1. Desenvolvimento</li>
-            <li>2. UI/UX design</li>
-            <li>3. Administração de conteúdo</li>
-            <li>4. Monitoria </li>
-          </ol>
-        </div>
-        <div
-          class="flex flex-col justify-center items-center p-5 flex m-10 rounded-tr-full rounded-bl-full bg-[#758BFD]">
-          <h1 class="p-5 text-5xl">SETOR DE RECURSOS</h1>
-          <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-1xl flex flex-col gap-5">
-            <li>1. Contabilidade</li>
-            <li>2. Mídias Digitais</li>
-            <li>3. Suporte</li>
-            <li>4. Feedback </li>
-          </ol>
-        </div>
+    <div class="flex">
+      <div class="flex flex-col justify-center items-center p-5 flex m-10 rounded-tr-full rounded-bl-full bg-[#758BFD]">
+        <h1 class="p-5 text-5xl">SETOR DE PRODUÇÃO</h1>
+        <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-1xl flex flex-col gap-5">
+          <li>1. Desenvolvimento</li>
+          <li>2. UI/UX design</li>
+          <li>3. Administração de conteúdo</li>
+          <li>4. Monitoria </li>
+        </ol>
       </div>
-      <Step fadeUp class="absolute z-20 gap-10  h-full rounded-xl w-full bg-[#758BFD] p-5">
-        <p class="text-5xl">AINDA FALANDO SOBRE ESTRUTURA , Nós somos uma sociedade limitada em que cada colaborador têm
-          por direito:</p>
+      <div class="flex flex-col justify-center items-center p-5 flex m-10 rounded-tr-full rounded-bl-full bg-[#758BFD]">
+        <h1 class="p-5 text-5xl">SETOR DE RECURSOS</h1>
+        <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-1xl flex flex-col gap-5">
+          <li>1. Contabilidade</li>
+          <li>2. Mídias Digitais</li>
+          <li>3. Suporte</li>
+          <li>4. Feedback </li>
+        </ol>
+      </div>
+    </div>
+  </Slide>
+  <Slide animate>
+    <h2 style="text-shadow: 0 10px 20px green;"
+      class="font-bold m-10 flex justify-center items-center text-green-600 gap-5 text-5xl">
+      <ToolIcon size="40" />ESTRUTURA
+    </h2>
+    <div class="flex justify-center items-center">
+      <div class="flex flex-col justify-center items-center p-5 flex rounded-tr-full rounded-bl-full bg-[#758BFD]">
+        <h1 class="p-5 text-5xl">AINDA FALANDO SOBRE ESTRUTURA , Nós somos uma sociedade limitada em que cada
+          colaborador têm por direito:</h1>
+        <h2 class="text-1xl"></h2>
         <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-1xl flex flex-col gap-5">
           <li>1. Porcentagem patrimonial</li>
           <li>2. Cotas mensais de acordo com a renda bruta da empresa</li>
-          <li>3. Opinião relevante em relação a planos de negócio e brainstorm</li>
+          <li>3. Opinião relevante em relação aos planos de negócio e o brainstorm</li>
         </ol>
-      </Step>
-    </Stack>
+      </div>
+    </div>
+  </Slide>
+    <Slide animate>
+    <h2 style="text-shadow: 0 10px 20px green;"
+      class="font-bold m-10 flex justify-center items-center text-green-600 gap-5 text-5xl">
+      <Share2Icon size="40" />EQUIPE
+    </h2>
+    <div class="flex justify-center items-center">
+      <div class="flex flex-col justify-center items-center p-5 flex rounded-tr-full rounded-bl-full bg-[#758BFD]">
+        <h1 class="p-5 text-5xl">NOSSA EQUIPE é composta por 3 pessoas físicas que exercem das mais variadas funções dento da empresa, dentre esses estão:</h1>
+        <h2 class="text-1xl"></h2>
+        <ol style="text-shadow: 0 10px 20px black;" class="p-5 text-1xl flex flex-col gap-5">
+          <li>1. FRANCISNÍ GOMES, DESENVOLVEDOR</li>
+          <li>2. FELYPE EMANUEL, DESIGNER DE INTERFACE</li>
+          <li>3. MATHEUS, ADM DE CONTEÚDO</li>
+        </ol>
+      </div>
+    </div>
+  </Slide>
+    <Slide animate class="flex'">
+    <div class="flex justify-around items-center">
+      <div class=" gap-5 p-5 flex justify-around items-center">
+        <h1 class="text-7xl font-bold text-start">MUITO <br /> OBRIGADO!</h1>
+        <img class="w-[20vw] rounded-full"
+          src="https://res.cloudinary.com/dt2se4cle/image/upload/v1693762648/Adobe_Express_20230901_1750260_1_orzusk.png"
+          alt="" />
+      </div>
+    </div>
+    <div class="p-10">
+      <h2 class="font-bold text-5xl">"Antes de ser ótimo em algo Eu preciso ser bom, antes de ser bom Eu preciso ser ruim, e antes de ser ruim tudo o que precisava era ter COMEÇADO"</h2>
+    </div>
+    
+    <a href="mailto:creastatic@gmail.com" class="py-10 text-xl flex items-center text-green-200 justify-center gap-5 overline">
+      <HelpCircleIcon /> Alguma dúvida, envie-nos um email
+    </a>
   </Slide>
 </Presentation>
